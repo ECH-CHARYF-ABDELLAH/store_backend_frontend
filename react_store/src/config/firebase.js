89925b345a37ddf99ae5442529eb0f8e9e2c293b
abdellah <116103813+ECH-CHARYF-ABDELLAH@users.noreply.github.com>
2026-04-19 01:10:35 +0100

    Clear Firebase config values for security
    Removed sensitive Firebase configuration details.

--- react_store/src/config/firebase.js
+++ react_store/src/config/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBhi8LKsog3K6r0UZ8fFS7VH1lqGNYz8QI",
-  authDomain: "ecommrce-72676.firebaseapp.com",
-  projectId: "ecommrce-72676",
+  apiKey: "",
+  authDomain: "",
+  projectId: "ecommrce-",
   storageBucket: "ecommrce-72676.firebasestorage.app",
   messagingSenderId: "369673601679",
-  appId: "1:369673601679:web:d550c3d1bbc1f94b5166fa",
-  measurementId: "G-604JR59BJ0"
+  appId: "1:369673601679:web:",
+  measurementId: "G-6049BJ0"
 };
 
 const app = initializeApp(firebaseConfig);
